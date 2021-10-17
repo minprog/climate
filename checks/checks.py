f7804b8ee31d2440f8bc887f941e0790a6c648a6
Jelleas <jelle.van.assema@gmail.com>
2021-10-17 15:08:52 +0200

    outlier check

--- checks/checks.py
+++ checks/checks.py
@@ -92,100 +92,51 @@ def variation_yearly(stdout):
         a["max_temp"] = int(a["max_temp"]) / 10
         text_float = "{} varieerde de temperatuur tussen {} op {} {} en {} op {} {}".format(*a.values())
 
-        if text_int not in stdout and text_float not in stdout:
-            text = "In " + text_float
-            raise check50.Failure(
-                f'kon "{text}" niet vinden in de output',
-                help="let extra goed op de spelling en of er niet te veel spaties staan"
-            )
+        find_any_prints([text_float, text_int], stdout)
 
 
 @check50.check(compiles)
 def longest_heatwave(stdout):
     """print de langste hittegolf voor ieder jaar"""
+    zero_days = "duurde de langste hittegolf 0 dagen"
+    if zero_days in stdout:
+        raise check50.Failure(
+            f'vond het volgende in de output: "{zero_days}"',
+            help="print geen jaartallen uit waarin er geen hittegolf heeft plaatsgevonden"
+        )
+
     check50.include("answers/longest_heatwave.csv")
     with open("longest_heatwave.csv") as f:
         answers = list(csv.DictReader(f))
 
     for a in answers:
         text = "{} duurde de langste hittegolf {} dagen".format(*a.values())
-
-        if text not in stdout:
-            text = "In " + text
-            raise check50.Failure(
-                f'kon "{text}" niet vinden in de output',
-                help="let extra goed op de spelling en of er niet te veel spaties staan"
-            )
-
-@check50.check(compiles)
-def correct_max_temp_day(stdout):
-    """prints the day of the maximum temperature"""
-    match = re.search("maximale temperatuur[^\d]*\d+[\.,]\d+[^\d]+(\d+) ([a-zA-Z]{3}) ([\d]{4})", stdout)
-
-    if not match:
-        raise check50.Failure("expected: De maximale temperatuur was XX.XX graden op XX XXX XXXX")
-
-    day = int(match.groups()[0])
-    month = match.groups()[1]
-    year = int(match.groups()[2])
-
-    if day != 27 or month.lower() != "jun" or year != 1947:
-        raise check50.Failure(f"expected 27 jun 1947 but found {day} {month} {year}")
+        find_any_prints([text], stdout)
 
 
 @check50.check(compiles)
-def correct_min_temp_day(stdout):
-    """prints the day of the minimum temperature"""
-    match = re.search("minimale temperatuur[^\d-]*-\d+[\.,]\d+[^\d]+(\d+) ([a-zA-Z]{3}) ([\d]{4})", stdout)
+def outliers(stdout):
+    """print de outliers voor ieder jaar"""
+    check50.include("answers/outliers.csv")
+    with open("outliers.csv") as f:
+        answers = list(csv.DictReader(f))
 
-    if not match:
-        raise check50.Failure("expected: De minimale temperatuur was -XX.XX graden op XX XXX XXXX")
+    for a in answers:
+        text_int = "{} is de waarde op {} {} {} een outlier".format(*a.values())
+        a["temp"] = int(a["temp"]) / 10
+        text_float = "{} is de waarde op {} {} {} een outlier".format(*a.values())
 
-    day = int(match.groups()[0])
-    month = match.groups()[1]
-    year = int(match.groups()[2])
-
-    if day != 20 or month.lower() != "dec" or year != 1938:
-        raise check50.Failure(f"expected 20 dec 1938 but found {day} {month} {year}")
-
-
-@check50.check(compiles)
-def correct_longest_freezing_period(stdout):
-    """prints the length of the longest freezing period"""
-    match = re.search("langste vriesperiode[^\d]*(\d+)", stdout)
-
-    if not match:
-        raise check50.Failure("expected: De langste vriesperiode is XX dagen")
-
-    days = int(match.groups()[0])
-
-    if days != 21:
-        raise check50.Failure(f"expected 21 days but found {days}")
+        find_any_prints([text_float, text_int], stdout)
 
 
-@check50.check(compiles)
-def correct_last_day_freezing(stdout):
-    """prints the last day of the longest freezing period"""
-    match = re.search("langste vriesperiode[^\d]*\d+[^\d]+(\d+) ([a-zA-Z]{3}) ([\d]{4})", stdout)
-
-    if not match:
-        raise check50.Failure("expected: De langste vriesperiode is XX dagen en eindigde op XX XXX XXXX")
-
-    day = int(match.groups()[0])
-    month = match.groups()[1]
-    year = int(match.groups()[2])
-
-    if day != 24 or month.lower() != "feb" or year != 1947:
-        raise check50.Failure(f"expected 24 feb 1947 but found {day} {month} {year}")
-
-    return stdout
-
-
-@check50.check(correct_last_day_freezing)
-def correct_year_heatwave(stdout):
-    """prints the year of the first heatwave"""
-    if "1911" not in stdout:
-        raise check50.Failure("expected 1911 as the year of the first heatwave")
+def find_any_prints(prints, stdout):
+    if all(p not in stdout for p in prints):
+        raise check50.Failure(
+            f'Kon "{prints[0]}" niet vinden in de output.',
+            help="Let extra goed op de spelling en of er niet te veel spaties staan.\n"\
+                    "    Print een dag in een datum altijd uit als twee karakters, bijvoorbeeld 03 juni 2019.\n"\
+                    "    Temperaturen mogen in tienden of hele graden Celsius worden geprint, bijvoorbeeld: 395 of 39.5"
+        )
 
 
 def find_int(prompt, number, text):
